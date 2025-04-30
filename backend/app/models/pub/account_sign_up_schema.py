a0b2b928b86f73adc01b9356b28681a766047e06
Daniel <Daniel328SignUp@gmail.com>
2025-04-30 12:44:52 +0100

    prefs

--- backend/app/models/pub/account_sign_up_schema.py
+++ backend/app/models/pub/account_sign_up_schema.py
@@ -1,7 +1,12 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, PositiveFloat
 
 class pSignUpData(BaseModel):
     first_name: str  # required
     last_name: str
     email_addr: EmailStr   # required
     password: str
+    annual_income: PositiveFloat
+    cash_invested: PositiveFloat
+    perc_contribution: PositiveFloat
+    employer_contribution: PositiveFloat
+    target_amount: PositiveFloat
